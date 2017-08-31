49ca71a681a7e704ea67730b603225185cf9efcb
Zack Kollar <rallokkcaz@gmail.com>
2017-08-31 13:42:54 -0700

    added git repository url to setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description = 'A command line app for keeping track of things and calculating stuff.',
     author = 'Zack Kollar',
     author_email = 'zackkollar@gmail.com',
-    # url = 'https://github.com/peterldowns/kalci', # use the URL to the github repo
+    url = 'https://github.com/SeedyROM/kalci', # use the URL to the github repo
     # download_url = 'https://github.com/peterldowns/kalci/archive/0.1.tar.gz', # I'll explain this in a second
     keywords = ['testing', 'logging', 'example'], # arbitrary keywords
     classifiers = [],
