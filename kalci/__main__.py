8d5a07d9107dd47670885ffd48356a5ea475183b
Zack Kollar <rallokkcaz@gmail.com>
2017-09-01 01:00:35 -0700

    Finishing for the night, here's a nice screen!

--- kalci/__main__.py
+++ kalci/__main__.py
@@ -33,12 +33,45 @@ def do_more_stuff(conn, cursor):
     tasks = cursor.execute('select rowid, task, completed, priority from tasks').fetchall()
     print(tasks)
 
+def test_urwid(terminal_colors):
+    import urwid
+
+    def exit_on_q(key):
+        if key.casefold() == 'q':
+            raise urwid.ExitMainLoop()
+
+    palette = [
+        ('banner', '', '', '', '#ffa', '#60d'),
+        ('streak', '', '', '', 'g50', '#60a'),
+        ('inside', '', '', '', 'g38', '#808'),
+        ('outside', '', '', '', 'g27', '#a06'),
+        ('bg', '', '', '', 'g7', '#d06'),]
+
+    placeholder = urwid.SolidFill()
+    loop = urwid.MainLoop(placeholder, palette, unhandled_input=exit_on_q)
+    loop.screen.set_terminal_properties(colors=256 if terminal_colors else 16)
+    loop.widget = urwid.AttrMap(placeholder, 'bg')
+    loop.widget.original_widget = urwid.Filler(urwid.Pile([]))
+
+    div = urwid.Divider()
+    outside = urwid.AttrMap(div, 'outside')
+    inside = urwid.AttrMap(div, 'inside')
+    txt = urwid.Text(('banner', u" KalCI == Here For U "), align='center')
+    streak = urwid.AttrMap(txt, 'streak')
+    pile = loop.widget.base_widget # .base_widget skips the decorations
+    for item in [outside, inside, streak, inside, outside]:
+        pile.contents.append((item, pile.options()))
+
+    loop.run()
+
 
 @click.command()
 @click.option('--path-to-db',
               default=settings.DATA_PATH,
               help='Location of kalci sqlite3 data...')
-def main(path_to_db):
+@click.option('--terminal-colors/--no-terminal-colors',
+               default=True)
+def main(path_to_db, terminal_colors):
     """Kalci's first point of execution, all the magic happens here.
     """
 
@@ -58,9 +91,12 @@ def main(path_to_db):
     # Create the instance of our db singleton.
     db(path_to_db)
 
-    # Test stuff.
-    do_stuff()
-    do_more_stuff()
+    # Test db stuff.
+    # do_stuff()
+    # do_more_stuff()
+
+    # Test interface code.
+    test_urwid(terminal_colors)
 
 # Execute our code.
 if __name__ == '__main__':
